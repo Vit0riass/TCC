03ab7078d446d28058ef616f483f989c98ba1e06
VITORIA SILVA SANTOS <00001082492772sp@aluno.educacao.sp.gov.br>
2024-11-06 15:39:21 -0300

    TCC COMPLETO

---
+++ js/index.js
@@ -0,0 +1,31 @@
+function mouseOverImg1() {
+    document.getElementById("teste1").src = "VITORIATCC/img/text1.png";
+}
+
+function mouseOutImg1() {
+    document.getElementById("teste1").src = "/img/cachorrob.png";
+}
+
+function mouseOverImg2() {
+    document.getElementById("teste2").src = "../img/text2.png";
+}
+
+function mouseOutImg2() {
+    document.getElementById("teste2").src = "/img/cachorrom.png";
+}
+
+function mouseOverImg3() {
+    document.getElementById("teste3").src = "img/text3.png";
+}
+
+function mouseOutImg3() {
+    document.getElementById("teste3").src = "/img/gatol.png";
+}
+
+function mouseOverImg4() {
+    document.getElementById("teste4").src = "/img/text4.png";
+}
+
+function mouseOutImg4() {
+    document.getElementById("teste4").src = "/img/gatom.png";
+}
